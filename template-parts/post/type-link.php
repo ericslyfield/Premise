554a9260e939e48077e9b212c52b5ee768e99c25
ericslyfield <40675543+ericslyfield@users.noreply.github.com>
2021-11-23 17:49:42 -0500

    Add files via upload
    Updated HTML Cascade for Link Post Format

--- template-parts/post/type-link.php
+++ template-parts/post/type-link.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- The Link Post Content -->
-<div class="content">
+<div class="content xx-large bold">
     <!-- Displays The First link in a Post As The Hyperlinked Title -->
-    <h2><a href=" <?php echo get_url_in_content(get_the_content())?>" target="_blank"> <?php the_title(); ?> </a></h2>
+    <a href=" <?php echo get_url_in_content(get_the_content())?>" target="_blank"> <?php the_title(); ?> </a>
 </div>
