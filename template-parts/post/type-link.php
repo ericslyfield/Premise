7c7c37220ccf96ee6f79b2689262823dacec1d22
ericslyfield <40675543+ericslyfield@users.noreply.github.com>
2021-11-24 13:22:48 -0500

    Add files via upload

--- template-parts/post/type-link.php
+++ template-parts/post/type-link.php
@@ -9,7 +9,7 @@
 ?>
 
 <!-- The Link Post Content -->
-<div class="content xx-large bold">
+<div class="content x-large bold">
     <!-- Displays The First link in a Post As The Hyperlinked Title -->
     <a href=" <?php echo get_url_in_content(get_the_content())?>" target="_blank"> <?php the_title(); ?> </a>
 </div>
